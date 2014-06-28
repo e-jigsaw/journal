160bff2ca6b25bd7ea5ef6187ccbde3e64b1d9ba
jigsaw <jigsaw@live.jp>
2014-06-29 02:47:15 +0900

    add send command

--- commands.go
+++ commands.go
@@ -8,11 +8,15 @@ import (
   "path"
   "time"
   "strconv"
+  "net/smtp"
+  "io/ioutil"
+  "encoding/json"
   "github.com/codegangsta/cli"
 )
 
 var Commands = []cli.Command {
   commandComment,
+  commandSend,
   commandWrite,
 }
 
@@ -22,6 +26,14 @@ var commandComment = cli.Command {
   Usage: "write comment",
   Description: "write comment",
   Action: doComment,
+}
+
+var commandSend = cli.Command {
+  Name: "send",
+  ShortName: "s",
+  Usage: "send mail",
+  Description: "send mail",
+  Action: doSend,
 }
 
 var commandWrite = cli.Command {
@@ -53,6 +65,50 @@ func doComment(c *cli.Context) {
     }
   }
   fout.WriteString("\n")
+}
+
+type Config struct {
+  Mail string
+  Pass string
+  To   string
+  Subj string
+}
+
+func doSend(c *cli.Context) {
+  usr, _ := user.Current()
+  conf, err := ioutil.ReadFile(path.Join(usr.HomeDir, ".journal.config.json"))
+  schedule, err := ioutil.ReadFile(path.Join(usr.HomeDir, ".journal"))
+  comment, err := ioutil.ReadFile(path.Join(usr.HomeDir, ".comment"))
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  var config Config
+  json.Unmarshal(conf, &config)
+  body := "To: " + config.To +
+    "\r\nSubject: " + config.Subj +
+    "\r\n\r\n# 本日の業務内容\n\ntime  | description\n----- | ----\n" + string(schedule) +
+    "\n# 所感\n\n" + string(comment) +
+    "\n---" +
+    "\nこの日報は激ヤバ鬼便利日報システム改( https://github.com/e-jigsaw/journal )によって送信されました\n"
+  auth := smtp.PlainAuth("", config.Mail, config.Pass, "smtp.gmail.com")
+  from := make([]string, 1)
+  from[0] = string(config.Mail)
+  err = smtp.SendMail("smtp.gmail.com:587", auth, config.To, from, []byte(body))
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  err = os.Remove(path.Join(usr.HomeDir, ".journal"))
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  err = os.Remove(path.Join(usr.HomeDir, ".comment"))
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
 }
 
 func doWrite(c *cli.Context) {
