7f6d9afd04b4d098bcdbd1adc4d45ea9ef826fed
jigsaw <jigsaw@live.jp>
2014-06-28 23:48:58 +0900

    add comment func

--- commands.go
+++ commands.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+  "fmt"
   "os"
   "os/user"
+  "bufio"
   "path"
   "time"
   "strconv"
@@ -10,7 +12,16 @@ import (
 )
 
 var Commands = []cli.Command {
+  commandComment,
   commandWrite,
+}
+
+var commandComment = cli.Command {
+  Name: "comment",
+  ShortName: "c",
+  Usage: "write comment",
+  Description: "write comment",
+  Action: doComment,
 }
 
 var commandWrite = cli.Command {
@@ -19,6 +30,20 @@ var commandWrite = cli.Command {
   Usage: "write journal",
   Description: "write journal",
   Action: doWrite,
+}
+
+func doComment(c *cli.Context) {
+  scanner := bufio.NewScanner(os.Stdin)
+  fmt.Print("Title: ")
+  for scanner.Scan() {
+    break
+  }
+  for i := 1; i <= 3; i++ {
+    fmt.Print("Comment", i, ": ")
+    for scanner.Scan() {
+      break
+    }
+  }
 }
 
 func doWrite(c *cli.Context) {
